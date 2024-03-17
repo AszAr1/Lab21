93c8058a71185b1600a4f42d487c280e1f67dfa2
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-03-17 22:01:13 +0300

    added func EnterA

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Lab21;
+
+class Program {
+    static double a;
+    static double b;
+    
+    public static void Main(string[] args) {
+        EnterA();
+    } 
+
+    public static void EnterA() {
+        Console.WriteLine("Enter a: ");
+        string str_a = Console.ReadLine()!;
+        while (!double.TryParse(str_a, out a)) {
+            Console.WriteLine("Try again");
+            str_a = Console.ReadLine()!;
+        }
+    }
+}
